a441e294c600bf35fc958b58e47e5982a0da3afa
ahmedragab13579 <ahmedharidy2019@gmail.com>
2026-03-30 19:27:18 +0200

    Last one

--- EcoBridgeAPI/Program.cs
+++ EcoBridgeAPI/Program.cs
@@ -104,10 +104,9 @@ builder.Services.AddRateLimiter(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
